2196f44092ed8b1d72d61f8917544a8433f9abb5
Fredrik Mile <fredrik.mile@gmail.com>
2022-02-10 08:23:50 +0100

    format multithreaded-exceptions.py

--- multithreaded-exceptions.py
+++ multithreaded-exceptions.py
@@ -10,6 +10,7 @@ LOGGER = logging.getLogger(__name__)
 def foo_with_exception(thread_id):
     raise Exception(f"Exception in thread id: {thread_id}")
 
+
 if __name__ == "__main__":
     LOGGER.info("Starting thread 0-4 using executor.map...")
     with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
@@ -19,10 +20,13 @@ if __name__ == "__main__":
                 LOGGER.info(result)
         except Exception as e:
             LOGGER.info(e)
-    
+
     LOGGER.info("Starting thread 5-9 using executor.submit...")
     with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-        future_to_thread = {executor.submit(foo_with_exception, thread_id): thread_id for thread_id in range(5,10)}
+        future_to_thread = {
+            executor.submit(foo_with_exception, thread_id): thread_id
+            for thread_id in range(5, 10)
+        }
         for future in concurrent.futures.as_completed(future_to_thread):
             try:
                 future.result()
